eb9b6c0d44854471eda79a179c5565b88baa11cf
Edward Brunton <Brunton@post.au.dk>
2019-02-19 13:07:08 +0100

    Added DoorBreachedTest

--- DoorControl.Test.Unit/DoorControlDoorBreachedSubstituteTests.cs
+++ DoorControl.Test.Unit/DoorControlDoorBreachedSubstituteTests.cs
@@ -1,10 +1,30 @@
+using System.Configuration;
+using System.Runtime.InteropServices;
+using DoorControl.Test.Unit.Mocks;
+using NSubstitute;
 using NUnit.Framework;
+
 
 namespace DoorControl.Test.Unit
 {
     [TestFixture]
     public class DoorControlDoorBreachedSubstituteTests
     {
+        private IDoorControlFactory _factory;
+        private DoorControl _uut;
         
+        [SetUp]
+        public void Setup()
+        {
+            _factory = Substitute.For<IDoorControlFactory>();
+            _uut = Substitute.For<DoorControl>();
+        }
+
+        [Test]
+        public void DoorBreached_DoorStateIsBreached()
+        {
+            _uut.DoorOpened();  // Breach door
+            _uut.Received().DoorOpened();
+        }
     }
 }
